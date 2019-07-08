c0cc0e3ba3c4456db77efc946ec594cd3fc1fe21
mayartmal <mayartmal@gmail.com>
2019-07-08 23:38:35 +0300

    range loop added

---
+++ range_loop.py
@@ -0,0 +1,9 @@
+friends = ['kot', 'bot', 'mot']
+congrats = 'Happy NY, '
+for friend in friends:
+    print(congrats + friend)
+
+print()
+
+for i in range(len(friends)):
+    print(congrats + friends[i])
